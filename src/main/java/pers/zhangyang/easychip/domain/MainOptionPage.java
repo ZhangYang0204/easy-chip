3d6c5783770aa13382508b50bc7eace72f8c3450
zhangyang <3319527246@qq.com>
2022-08-23 17:04:38 +0800

    1

--- src/main/java/pers/zhangyang/easychip/domain/MainOptionPage.java
+++ src/main/java/pers/zhangyang/easychip/domain/MainOptionPage.java
@@ -23,7 +23,7 @@ import java.util.List;
 
 public class MainOptionPage extends SingleGuiPageBase implements BackAble {
     public MainOptionPage(Player viewer, GuiPage backPage, OfflinePlayer owner) {
-        super(GuiYaml.INSTANCE.getString("gui.title.mainOptionPage"), viewer, backPage, owner);
+        super(GuiYaml.INSTANCE.getString("gui.title.mainOptionPage"), viewer, backPage, owner,54);
     }
 
     @Override
@@ -33,6 +33,11 @@ public class MainOptionPage extends SingleGuiPageBase implements BackAble {
             return;
         }
         CommandUtil.dispatchCommandList(viewer, cmdList);
+    }
+
+    @Override
+    public int getBackSlot() {
+        return 49;
     }
 
     @Override
